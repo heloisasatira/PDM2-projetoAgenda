b25e5b76581933582392e3514c55c993bd6532f6
Heloisa S. Rios <heloisasatirariosdossantos@gmail.com>
2024-10-26 19:46:14 -0300

    Criação das páginas inicial, de aulas e de PE's,

---
+++ sw.js
@@ -0,0 +1,18 @@
+let cacheName = "minha-agenda";
+let filesToCache = ["/", "/index.html", "/index2.html", "/index3.html", "/css/style.css", "/js/main.js"];
+
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
